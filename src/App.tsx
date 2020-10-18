501b8e83312c26eb58a601933f028ef0bbfecc3e
Steven <gonghaima@hotmail.com>
2020-10-18 19:58:56 +1100

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 import './App.scss'
 
 const App: React.StatelessComponent<{}> = () => (
-    <div className='container'>
-        <h1>Hello World, React!</h1>
-    </div>
+  <div className='container'>
+    <h1>Hello World, React!</h1>
+  </div>
 )
 
-export default App;
+export default App
